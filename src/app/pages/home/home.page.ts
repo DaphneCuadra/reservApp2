996b519c646f7ebd08a177e0cea36f2474867aca
DaphneCuadra <dap.cuadra@duocuc.cl>
2024-10-10 12:26:44 -0300

    Guard listo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  //Vamos a crear una variable que almacenará la información del localStorage
+  usuario: any;
+
+  constructor(private navController: NavController) {}
+
+  ngOnInit(){
+    this.usuario = JSON.parse(localStorage.getItem("usuario") || '');
+    //console.log(this.usuario); Para pruebas
+  }
+
+  logout(){
+    localStorage.removeItem('usuario');
+    this.navController.navigateRoot('/login');
+  }
 
 }
